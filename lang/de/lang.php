a43a9156cf1357ab0b68acf009beba9af16911fb
Benjamin Vetter <Benjamin Vetter>
2016-12-06 18:12:41 +0100

    german language files added

---
+++ lang/de/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* German translation for the authphpbb3 plugin.
+*
+* @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author   Benjamin Vetter <mail@b-vetter.de>
+*/
+
+$lang['config_error']       = 'Konfigurations Fehler: Kontaktiere bitte einen Administrator.';
+$lang['database_error']     = 'Datenbank Fehler: Kontaktiere bitte einen Administrator.';
+$lang['login_login']        = 'Benutzername';
+$lang['login_password']     = 'Passwort';
+$lang['login_remember']     = 'Merken';
+$lang['login_viewonline']   = 'Verstecke meinen Online Status';
+$lang['login_button']       = 'Login';
+$lang['login_bottom_text']  = 'Du kannst dich mit deinem <a href="%s" class="urlextern">Forum Account</a> einloggen.';
